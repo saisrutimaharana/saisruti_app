79df73146d4e23ec76f9026e808efb93e0cb145f
saisrutimaharana <maharanasaisruti@gmail.com>
2023-10-01 20:23:26 +0530

    create

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  devise_scope :user do
-    get 'users/new_special', to: 'users/registrations#new_special'
-    post 'users/create_special', to: 'users/registrations#create_special'
-  end
+  devise_for :users
+  # controllers: {
+  #   sessions: 'users/sessions',
+  #   registrations: 'users/registrations'
+  # }
+  # devise_scope :user do
+  #   get 'users/new_special', to: 'users/registrations#new_special'
+  #   post 'users/create_special', to: 'users/registrations#create_special'
+  # end
+  resources :posts
+  root to:'posts#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
